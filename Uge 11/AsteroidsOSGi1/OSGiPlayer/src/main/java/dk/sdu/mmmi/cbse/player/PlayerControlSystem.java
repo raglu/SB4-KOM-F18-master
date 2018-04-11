b6a08f0b2508aa57534959ae40dc53c6751a14be
raglu <raglu16@student.sdu.dk>
2018-04-11 16:19:45 +0200

    arbejdet lidt mud uge 11

---
+++ Uge 11/AsteroidsOSGi1/OSGiPlayer/src/main/java/dk/sdu/mmmi/cbse/player/PlayerControlSystem.java
@@ -0,0 +1,77 @@
+package dk.sdu.mmmi.cbse.player;
+
+import dk.sdu.mmmi.cbse.common.bullet.BulletSPI;
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import static dk.sdu.mmmi.cbse.common.data.GameKeys.LEFT;
+import static dk.sdu.mmmi.cbse.common.data.GameKeys.RIGHT;
+import static dk.sdu.mmmi.cbse.common.data.GameKeys.SPACE;
+import static dk.sdu.mmmi.cbse.common.data.GameKeys.UP;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
+import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
+import dk.sdu.mmmi.cbse.common.player.Player;
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+
+public class PlayerControlSystem implements IEntityProcessingService {
+
+    private BulletSPI bulletService;
+
+
+    @Override
+    public void process(GameData gameData, World world) {
+
+        for (Entity player : world.getEntities(Player.class)) {
+
+            PositionPart positionPart = player.getPart(PositionPart.class);
+            MovingPart movingPart = player.getPart(MovingPart.class);
+            
+            movingPart.setLeft(gameData.getKeys().isDown(LEFT));
+            movingPart.setRight(gameData.getKeys().isDown(RIGHT));
+            movingPart.setUp(gameData.getKeys().isDown(UP));
+            
+            if (gameData.getKeys().isDown(SPACE)) {
+                Entity bullet = bulletService.createBullet(player, gameData);
+                world.addEntity(bullet);
+            }
+            
+            movingPart.process(gameData, player);
+            positionPart.process(gameData, player);            
+            updateShape(player);
+
+        }
+    }
+
+    private void updateShape(Entity entity) {
+        float[] shapex = entity.getShapeX();
+        float[] shapey = entity.getShapeY();
+        PositionPart positionPart = entity.getPart(PositionPart.class);
+        float x = positionPart.getX();
+        float y = positionPart.getY();
+        float radians = positionPart.getRadians();
+
+        shapex[0] = (float) (x + Math.cos(radians) * 8);
+        shapey[0] = (float) (y + Math.sin(radians) * 8);
+
+        shapex[1] = (float) (x + Math.cos(radians - 4 * 3.1415f / 5) * 8);
+        shapey[1] = (float) (y + Math.sin(radians - 4 * 3.1145f / 5) * 8);
+
+        shapex[2] = (float) (x + Math.cos(radians + 3.1415f) * 5);
+        shapey[2] = (float) (y + Math.sin(radians + 3.1415f) * 5);
+
+        shapex[3] = (float) (x + Math.cos(radians + 4 * 3.1415f / 5) * 8);
+        shapey[3] = (float) (y + Math.sin(radians + 4 * 3.1415f / 5) * 8);
+
+        entity.setShapeX(shapex);
+        entity.setShapeY(shapey);
+    }
+
+    //TODO: Dependency injection via Declarative Services
+    public void setBulletService(BulletSPI bulletService) {
+        this.bulletService = bulletService;
+    }
+
+    public void removeBulletService() {
+        this.bulletService = null;
+    }
+}
